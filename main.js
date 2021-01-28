a4586d16d1f175de319d879e455c50a10fb402f5
Robby Noyes <robertjnoyes1@gmail.com>
2021-01-28 13:32:44 -0800

    seperated functions, cleaner look, adding score

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissors = document.querySelector('#scissors')
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-rock.addEventListener('click', playRound)
-paper.addEventListener('click', playRound)
-scissors.addEventListener('click', playRound)
-
-
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
 
 function computerPlay() {
-    let random = Math.random();
-    if (random < 0.33) {
-        return "Rock";
-    } else if (random < 0.66) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
+  let random = Math.random();
+  if (random < 0.33) {
+    return "Rock";
+  } else if (random < 0.66) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
-function playRound(e){
-    let computerSelection = computerPlay();
-    let playerSelection = e.target.id
+function playRound(e) {
+  let computerSelection = computerPlay().toLowerCase();
+  let playerSelection = e.target.id;
 
-    let playerWin, computerWin = 0
-
-    if((playerSelection === 'rock' && computerSelection === 'Scissors') || (playerSelection === 'paper' && computerSelection === 'Rock') || (playerSelection === 'scissors' && computerSelection === 'Paper')){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-        playerWin++
-    } else {
-        computerWin++
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
-    }
-    return playerWin, computerWin
+  choice(playerSelection, computerSelection);
 }
 
-let resultsPlayer = document.querySelector('#playWin').innerText = playerWin
-let resultsComputer = document.querySelector('#compWin').innerText = computerWin
+let playerWin,
+  computerWin = 0;
+
+let resultsPlayer = document.querySelector("#playWin");
+let resultsComputer = document.querySelector("#compWin");
+
+function choice(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "Scissors") ||
+    (playerSelection === "paper" && computerSelection === "Rock") ||
+    (playerSelection === "scissors" && computerSelection === "Paper")
+  ) {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    playerWin++;
+  } else if (playerSelection === computerSelection) {
+    console.log(`You tied!`);
+  } else {
+    console.log(`You Lost! ${computerSelection} beats ${playerSelection}`);
+    computerWin++;
+  }
+}
