cefad807f801726908b1741de6fba1fb60c4eba6
Robby Noyes <robertjnoyes1@gmail.com>
2021-01-28 10:40:46 -0800

    Add playRound for round against bot

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-function computerPlay(){
-    let random = Math.random()
-    return random <= 0.33 ? 'Rock' 
-         : random <= 0.66 ? 'Paper'
-         : random <= 1 ? 'Scissors'
-         : 'ERROR'
+function computerPlay() {
+  let random = Math.random();
+  return random <= 0.33
+    ? "Rock"
+    : random <= 0.66
+    ? "Paper"
+    : random <= 1
+    ? "Scissors"
+    : "ERROR";
 }
 
+function playRound(playerSelection, computerSelection) {
+  return (playerSelection === "rock" && computerSelection === "Paper") ||
+    (playerSelection === "paper" && computerSelection === "Rock") ||
+    (playerSelection === "scissors" && computerSelection === "Paper")
+    ? `You win! ${playerSelection} beats ${computerSelection}`
+    : `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+
+let playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+
+
+console.log(playerSelection)
+
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection))
