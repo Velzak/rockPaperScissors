cc55bc82b600f65ff4de4b1c172823e7ef2731b0
Robby Noyes <robertjnoyes1@gmail.com>
2021-01-28 10:22:24 -0800

    Create computerPlay for random gen of answer

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function computerPlay(){
     let random = Math.random()
-
-    
+    return random <= 0.33 ? 'Rock' 
+         : random <= 0.66 ? 'Paper'
+         : random <= 1 ? 'Scissors'
+         : 'ERROR'
 }
+
